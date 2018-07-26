221ca32ccfb5a831c4e90b2be3114811b8bba137
m_p <yamada31290@live.jp>
2018-07-26 23:28:21 +0900

    アーカイブ一覧のリンクで、本文を削除しPCのみ「動画」「CD」「ライブ情報」へのリンクを追加する。

---
+++ theme/module_loop_post_music.php
@@ -0,0 +1,28 @@
+﻿<article class="media">
+<div id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+	<?php if ( has_post_thumbnail()) :?>
+	<div class="media-left postList_thumbnail">
+		<a href="<?php the_permalink(); ?>">
+		<?php
+		$attr = array('class'	=> "media-object");
+		the_post_thumbnail('thumbnail',$attr); ?>
+		</a>
+	</div>
+	<?php endif; ?>
+	<div class="media-body">
+		<?php get_template_part('module_loop_post_meta');?>
+		<h1 class="media-heading entry-title"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h1>
+<div class="media-body_excerpt">
+
+<?php if(!wp_is_mobile()): ?>
+<a href="<?php the_permalink(); ?>#movie"  id="button">公式動画を見る</a>
+<a href="<?php the_permalink(); ?>#CD"  id="button">収録CDを見る</a>
+<a href="<?php the_permalink(); ?>#live"  id="button">ライブ情報を見る</a>
+<?php endif; ?>
+</div>
+		<!--
+		<div><a href="<?php the_permalink(); ?>" class="btn btn-default btn-sm"><?php _e('Read more', 'lightning'); ?></a></div>
+		-->   
+	</div>
+</div>
+</article>
