e477e0d44765a74c4082162d8975439dc591e121
Macc-P <maccha@fujiwarahaji.me>
2018-11-24 16:53:55 +0900

    CSS読み込みで変数指定していない場合404ログが溜まるので、404にならないよう改善。

--- theme/taxonomy-idol_cg.php
+++ theme/taxonomy-idol_cg.php
@@ -14,7 +14,9 @@ get_template_part('sitehensu/shiny');
 <?php get_template_part('module_pageTit'); ?>
 <?php get_template_part('module_panList'); ?>
 
+<?php if(!empty($css_pass)):?>
 <link rel="stylesheet" href="<?php echo get_stylesheet_directory_uri(); ?>/css/box/<?php echo $css_pass; ?>.css" type="text/css" />
+<?php endif; ?>
 
 <div class="section siteContent">
 <div class="container">
