806ea45e23653b54152a0754d30d9a3f4e793767
Macc-P <maccha@fujiwarahaji.me>
2019-05-24 23:09:24 +0900

    CD一覧をある程度自動で分類できるように設定

---
+++ theme/parts/sitemap/disclist.php
@@ -0,0 +1,149 @@
+﻿<?php
+$disc_series = array(
+    //array('name'=>'見出しとして表示するseries名' , 'word'=>'検索ワード' , 'type'=>'cg ml sc radio as')
+    //array('name'=>'' , 'word'=>'' 'type'=>'')
+    array("name"=>"CINDERELLA MASTER ソロCD" , "word"=>"THE IDOLM@STER CINDERELLA MASTER 0" , "type"=>"cg") ,
+    array("name"=>"CINDERELLA MASTER ユニット・カバーCD" , "word"=>"THE IDOLM@STER CINDERELLA MASTER" , "type"=>"cg") ,
+    array("name"=>"STARLIGHT MASTER" , "word"=>"THE IDOLM@STER CINDERELLA GIRLS STARLIGHT MASTER" , "type"=>"cg") ,
+    array("name"=>"MASTER SEASONS" , "word"=>"THE IDOLM@STER CINDERELLA GIRLS MASTER SEASONS" , "type"=>"cg") ,
+    array("name"=>"シンデレラガールズアニメ1期" , "word"=>"THE IDOLM@STER CINDERELLA GIRLS ANIMATION PROJECT 0" , "type"=>"cg") ,
+    array("name"=>"シンデレラガールズアニメ2期" , "word"=>"THE IDOLM@STER CINDERELLA GIRLS ANIMATION PROJECT 2nd" , "type"=>"cg") ,
+    array("name"=>"346Pro IDOL selection" , "word"=>"346Pro IDOL selection" , "type"=>"cg") ,
+    array("name"=>"シンデレラガールズアニメCD その他" , "word"=>"THE IDOLM@STER CINDERELLA GIRLS ANIMATION" , "type"=>"cg") ,
+    array("name"=>"LITTLE STARS!（しんげき）" , "word"=>"THE IDOLM@STER CINDERELLA GIRLS LITTLE STARS!" , "type"=>"cg") ,
+    array("name"=>"WILD WIND GIRL" , "word"=>"THE IDOLM@STER CINDERELLA GIRLS WILD WIND GIRL" , "type"=>"cg") ,
+    array("name"=>"U149" , "word"=>"THE IDOLM@STER CINDERELLA GIRLS U149" , "type"=>"cg") ,
+    array("name"=>"After20" , "word"=>"THE IDOLM@STER CINDERELLA GIRLS After20" , "type"=>"cg") ,
+    array("name"=>"その他シンデレラガールズ（単発、ライブ限定盤）" , "word"=>"THE IDOLM@STER CINDERELLA GIRLS" , "type"=>"cg") ,
+
+    array("name"=>"LIVE THE@TER PERFORMANCE" , "word"=>"THE IDOLM@STER LIVE THE@TER PERFORMANCE" , "type"=>"ml") ,
+    array("name"=>"LIVE THE@TER HARMONY" , "word"=>"THE IDOLM@STER LIVE THE@TER HARMONY" , "type"=>"ml") ,
+    array("name"=>"LIVE THE@TER DREAMERS" , "word"=>"THE IDOLM@STER LIVE THE@TER DREAMERS" , "type"=>"ml") ,
+    array("name"=>"THE@TER ACTIVITIES" , "word"=>"THE IDOLM@STER THE@TER ACTIVITIES" , "type"=>"ml") ,
+    array("name"=>"LIVE THE@TER FORWARD" , "word"=>"THE IDOLM@STER LIVE THE@TER FORWARD" , "type"=>"ml") ,
+    array("name"=>"MILLION THE@TER GENERATION" , "word"=>"THE IDOLM@STER MILLION THE@TER GENERATION" , "type"=>"ml") ,
+    array("name"=>"M@STER SPARKLE" , "word"=>"THE IDOLM@STER MILLION LIVE! M@STER SPARKLE" , "type"=>"ml") ,
+    array("name"=>"THE@TER BOOST" , "word"=>"THE IDOLM@STER THE@TER BOOST" , "type"=>"ml") ,
+    array("name"=>"LIVE THE@TER SOLO COLLECTION（ソロコレ）" , "word"=>"THE IDOLM@STER LIVE THE@TER SOLO COLLECTION" , "type"=>"ml") ,
+    array("name"=>"ゲッサン版ミリオンライブ！" , "word"=>"ゲッサン版ミリオンライブ！" , "type"=>"ml") ,
+    array("name"=>"Blooming Clover" , "word"=>"アイドルマスター ミリオンライブ！ Blooming Clover" , "type"=>"ml") ,
+    array("name"=>"その他ミリオンライブ！" , "word"=>"THE IDOLM@STER MILLION LIVE!" , "type"=>"ml") ,
+    array("name"=>"その他ミリオンライブ！" , "word"=>"THE IDOLM@STER LIVE THE@TER" , "type"=>"ml") ,
+
+    array("name"=>"BRILLI@NT WING" , "word"=>"THE IDOLM@STER SHINY COLORS BRILLI@NT WING" , "type"=>"sc") ,
+    array("name"=>"FR@GMENT WING" , "word"=>"THE IDOLM@STER SHINY COLORS FR@GMENT WING" , "type"=>"sc") ,
+    array("name"=>"SOLO COLLECTION（ソロコレ）" , "word"=>"THE IDOLM@STER SHINY COLORS SOLO COLLECTION" , "type"=>"sc") ,
+    array("name"=>"その他シャイニーカラーズ" , "word"=>"THE IDOLM@STER SHINY COLORS" , "type"=>"sc") ,
+
+    array("name"=>"GRE@TEST BEST!" , "word"=>"THE IDOLM@STER 765PRO ALLSTARS+ GRE@TEST BEST!" , "type"=>"as") ,
+    array("name"=>"ANIM@TION MASTER" , "word"=>"THE IDOLM@STER ANIM@TION MASTER" , "type"=>"as") ,
+    array("name"=>"BEST ALBUM" , "word"=>"THE IDOLM@STER BEST" , "type"=>"as") ,
+    array("name"=>"MASTER ARTIST" , "word"=>"THE IDOLM@STER MASTER ARTIST" , "type"=>"as") ,
+    array("name"=>"MASTER SPECIAL" , "word"=>"THE IDOLM@STER MASTER SPECIAL" , "type"=>"as") ,
+    array("name"=>"MASTERPIECE" , "word"=>"THE IDOLM@STER MASTERPIECE" , "type"=>"as") ,
+    array("name"=>"映画関係" , "word"=>"THE IDOLM@STER MOVIE" , "type"=>"as") ,
+    array("name"=>"STELLA MASTER" , "word"=>"THE IDOLM@STER STELLA MASTER" , "type"=>"as") ,
+
+    array("name"=>"MOIW関係" , "word"=>"THE IDOLM@STER M@STERS OF IDOL WORLD!!" , "type"=>"as") ,
+    array("name"=>"デレパ" , "word"=>"CINDERELLA PARTY!" , "type"=>"radio") ,
+    array("name"=>"作者によるカバー" , "word"=>"☆" , "type"=>"cover") ,
+    array("name"=>"他アーティストによるカバー" , "word"=>"△" , "type"=>"cover") ,
+
+
+
+
+
+);
+
+?>
+<p>CD名からの自動分類を実験中です。<br>
+
+シンデレラガールズ楽曲は<a href="https://cg-portal.net">CG-Portalさん</a>からもCD検索ができます。</p>
+
+<h3>ページ内ショートカット</h3>
+<ul>
+<?php
+foreach($disc_series as $S_No => $series){
+    echo '  <li><a href="#disc'.$S_No.'">'.$series[name].'</a></li>';
+    echo PHP_EOL;
+
+}
+?>
+</ul>
+
+<?php
+// カスタム分類名
+$taxonomy = 'disc';
+
+// パラメータ 
+$args = array(
+    // 子タームの投稿数を親タームに含める
+    'pad_counts' => true,
+  
+    // 投稿記事がないタームも取得
+    'hide_empty' => false
+);
+
+// カスタム分類のタームのリストを取得
+$terms = get_terms( $taxonomy , $args );
+
+if ( count( $terms ) != 0 ) {
+     
+    // タームのリスト $terms を $term に格納してループ
+    foreach ( $terms as $term ) {
+        foreach ( $disc_series as $series ){
+            $search = '/^'.$series[word].'/';
+            $series_name = $series[name];
+            if ( preg_match( $search , $term->name ) ){
+                ${$series_name}[] = $term;
+                continue 2;
+            }
+        }
+        $else_term[] = $term;
+    }
+//最後の処理
+}
+
+foreach($disc_series as $S_No => $series){
+    echo '<h2 class="'.$series[type].'" id="disc'.$S_No.'">'.$series[name]."</h2>";
+    echo PHP_EOL;
+    echo "<ul>";
+    echo PHP_EOL;
+
+    foreach (${$series[name]} as $term){
+        $term = sanitize_term( $term, $taxonomy );
+        $term_link = get_term_link( $term, $taxonomy );
+        if ( is_wp_error( $term_link ) ) {
+            continue;
+        }
+        echo '  <li><a href="'.$term_link.'">'.$term->name.'</a></li>';
+        echo PHP_EOL;
+
+    }
+    echo "</ul>";
+    echo PHP_EOL;
+    echo PHP_EOL;
+
+
+}
+
+echo '<h2  class="else" >未分類</h2>';
+echo PHP_EOL;
+
+echo "<ul>";
+echo PHP_EOL;
+
+foreach ($else_term as $term){
+    $term = sanitize_term( $term, $taxonomy );
+    $term_link = get_term_link( $term, $taxonomy );
+    if ( is_wp_error( $term_link ) ) {
+        continue;
+    }
+    echo '  <li><a href="'.$term_link.'">'.$term->name.'</a></li>';
+    echo PHP_EOL;
+
+}
+echo "</ul>";
+
+?>
+
