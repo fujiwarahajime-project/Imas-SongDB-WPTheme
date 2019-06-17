9edf1c0804e139b3c66fcc1a5a5b716aac5bc354
Macc-P <maccha@fujiwarahaji.me>
2019-06-17 16:48:28 +0900

    METAサムネイルの曲ページ対応

--- theme/parts/music_page/core.php
+++ theme/parts/music_page/core.php
@@ -60,6 +60,15 @@ if(is_singular( 'music_cg' )){ //シンデレラガールズの場合
 	echo get_bloginfo( 'template_directory' ) . '/images/thumbnail.png';
 } ?>
 ">
+<meta property="thumbnail" content="<?php if ( has_post_thumbnail() ) {
+	$image_id = get_post_thumbnail_id ();
+	$image_url = wp_get_attachment_image_src ($image_id, true);
+	echo $image_url[0];
+} else {
+	echo get_bloginfo( 'template_directory' ) . '/images/thumbnail.png';
+} ?>
+">
+
 
 <style>
 .solo{
